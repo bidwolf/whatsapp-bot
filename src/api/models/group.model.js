74e8dfcda9fee971e620a261768218c94b00c9d2
Henrique de Paula Rodrigues <henrique@MKB-HENRIQUE.local>
2024-10-31 14:28:47 -0300

    feat: add allow offenses in groupSchema in mongoDB

--- src/api/models/group.model.js
+++ src/api/models/group.model.js
@@ -1,24 +1,37 @@
-const mongoose = require('mongoose')
-
+const mongoose = require("mongoose");
+/**
+ * @typedef IGroup
+ * @property {string} key - The key of the group
+ * @property {string} groupId - The id of the group
+ * @property {array} blockedCommands - The commands that are blocked in the group
+ * @property {array} blackListedUsers - The users that are blacklisted in the group
+ * @property {boolean} allowOffenses - The flag that indicates if the group allows offenses
+ */
 const groupSchema = new mongoose.Schema({
-    key: {
-        type: String,
-        required: [true, 'key is missing'],
-        unique: true,
-    },
-    groupId: {
-        type: String,
-        required: [true, 'id is missing'],
-        unique: true,
-    },
-    blockedCommands:{
-        type: Array,
-    },
-    blackListedUsers:{
-        type: Array,
-    },
-})
+  key: {
+    type: String,
+    required: [true, "key is missing"],
+    unique: true,
+  },
+  groupId: {
+    type: String,
+    required: [true, "id is missing"],
+    unique: true,
+  },
+  blockedCommands: {
+    type: Array,
+    default: [],
+  },
+  blackListedUsers: {
+    type: Array,
+    default: [],
+  },
+  allowOffenses: {
+    type: Boolean,
+    default: false,
+  },
+});
 
-const Group = mongoose.model('Group', groupSchema)
+const Group = mongoose.model("Group", groupSchema);
 
-module.exports = Group
+module.exports = Group;
