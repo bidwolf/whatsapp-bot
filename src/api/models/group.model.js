f16575783ec3b99ef7f4e38a3eb328a8caeb3185
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2024-11-10 01:07:38 -0300

    refactor: update MongoDB connection to use global scope and clean up code

--- src/api/models/group.model.js
+++ src/api/models/group.model.js
@@ -1,18 +1,12 @@
 const mongoose = require("mongoose");
 /**
  * @typedef IGroup
- * @property {string} key - The key of the group
  * @property {string} groupId - The id of the group
  * @property {array} blockedCommands - The commands that are blocked in the group
  * @property {array} blackListedUsers - The users that are blacklisted in the group
  * @property {boolean} allowOffenses - The flag that indicates if the group allows offenses
  */
 const groupSchema = new mongoose.Schema({
-  key: {
-    type: String,
-    required: [true, "key is missing"],
-    unique: true,
-  },
   groupId: {
     type: String,
     required: [true, "id is missing"],
@@ -30,7 +24,7 @@ const groupSchema = new mongoose.Schema({
     type: Boolean,
     default: false,
   },
-  messages: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Message' }] // Relacionar mensagens
+  messages: [{ type: mongoose.Schema.Types.ObjectId, ref: "Message" }], // Relacionar mensagens
 });
 
 const Group = mongoose.model("Group", groupSchema);
