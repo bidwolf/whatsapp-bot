372e8858b428aae9c3dac5bdb7abbb415c22c18c
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2024-11-15 07:19:18 -0300

    feat: creating command validator to validate permission based command

---
+++ src/validators/ValidatePermission.ts
@@ -0,0 +1,13 @@
+import { CommandValidator, ValidateProps } from '.';
+import { getWhatsAppId } from '../utils/getWhatsappId';
+export type ParticipantPermission = 'admin' | 'superadmin' | null | undefined
+export default abstract class ValidatePermission implements CommandValidator {
+  async validate({ command, metadata }: ValidateProps): Promise<Boolean> {
+    if (!this.permission) return true // Usuário não precisa de nenhum tipo de permissão
+    if (!command || !command.groupId || !command.command_executor || !metadata) return false
+    const executorWhatsappId = getWhatsAppId(command.command_executor)
+    const isAdmin = metadata.participants.find(participant => participant.id === executorWhatsappId)?.admin
+    return Boolean(isAdmin)
+  }
+  constructor(private readonly permission: ParticipantPermission) { }
+}
