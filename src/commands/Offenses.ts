6615d73e2ca05d1d822d297418d698542c26e2bb
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2024-11-16 10:03:47 -0300

    refactor: now toggle offenses command uses validationRunner implementation

--- src/commands/Offenses.ts
+++ src/commands/Offenses.ts
@@ -1,11 +1,12 @@
 
 import pino from 'pino';
-import { BaseCommand, Method, validateCommandProps } from '../utils/commands';
+import { BaseCommand } from '../utils/commands';
 import { ExtendedWAMessageUpdate, ExtendedWaSocket } from '../utils/messageTransformer';;
-import { GroupMetadata } from '@whiskeysockets/baileys';
 import Group from '../api/models/group.model';
 import { COMMAND_PREFIX } from '../utils/constants';
-import { getWhatsAppId } from '../utils/getWhatsappId';
+import ValidationRunner from '../validators/ValidationRunner';
+import ValidateMethods from '../validators/ValidateMethods';
+import ValidateExecutorAdmin from '../validators/ValidateExecutorAdmin';
 export default class Offenses extends BaseCommand {
   private async toggleAllowOffenses(groupId: string, turnOn: boolean) {
     try {
@@ -25,24 +26,21 @@ export default class Offenses extends BaseCommand {
     return false
   }
   async execute(message: ExtendedWAMessageUpdate, instance: ExtendedWaSocket): Promise<void> {
-    const { command, method } = message
-    if (!command) {
-      throw new Error('Command not found')
-    }
-    if (!method) {
-      throw new Error('Method not found')
-    }
-    const groupMetadata = await this.validateCommand({ command, method, instance })
-    if (!groupMetadata) {
-      return
-    }
-    if (command.groupId) {
-      let toggleCommandArgs = command.args
-      if (command.args && typeof command.args === 'object') {
-        toggleCommandArgs = command.args[0]
+    const groupMetadata = await instance.groupMetadata(message.command?.groupId || '')
+    const valid = await this.validator.runValidations({
+      command: message.command,
+      metadata: groupMetadata,
+      method: message.method,
+      reply: message.reply
+    })
+    if (!valid) return
+    if (message.command?.groupId) {
+      let toggleCommandArgs = message.command?.args
+      if (message.command?.args && typeof message.command?.args === 'object') {
+        toggleCommandArgs = message.command?.args[0]
       }
       if (toggleCommandArgs === 'on' || toggleCommandArgs === 'off') {
-        const allowOffensesStatus = await this.toggleAllowOffenses(command.groupId, toggleCommandArgs === 'on')
+        const allowOffensesStatus = await this.toggleAllowOffenses(message.command?.groupId, toggleCommandArgs === 'on')
         if (allowOffensesStatus && message.reply) {
           message.reply(`Ofensas ${toggleCommandArgs === 'on' ? 'ativadas' : 'desativadas'} com sucesso`)
         }
@@ -55,30 +53,9 @@ export default class Offenses extends BaseCommand {
     }
   }
   private readonly logger = pino()
-  private readonly allowedMethods: Method[] = ['raw']
-  async validateCommand(props: validateCommandProps): Promise<GroupMetadata | null> {
-    if (!props.command.groupId) {
-      throw new Error('Group ID not found')
-    }
-    if (!props.command.command_executor) {
-      throw new Error('Command executor not found')
-    }
-
-    const whatsAppId = getWhatsAppId(props.command.command_executor)
-
-    const groupMetadata = await props.instance.groupMetadata(props.command.groupId)
-    if (!groupMetadata) {
-      throw new Error('Group metadata not found')
-    }
-    const isAdmin = groupMetadata.participants.find(p => p.id === whatsAppId)?.admin
-    if (isAdmin) {
-      const isAllowed = this.allowedMethods.includes(props.method)
-      this.logger.info(`Method ${isAllowed ? '' : 'not'} allowed for user ${whatsAppId}`)
-      return isAllowed ? groupMetadata : null
-    }
-    return null
-  }
   constructor() {
-    super('ofensa')
+    const adminValidator = new ValidateExecutorAdmin()
+    const methodValidator = new ValidateMethods(['raw'])
+    super('ofensa', new ValidationRunner([adminValidator, methodValidator]))
   }
 }
