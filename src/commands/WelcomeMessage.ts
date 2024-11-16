cd1cdca816f63cc22ec7f3cee4e9c8d7950cd42e
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2024-11-16 10:07:11 -0300

    refactor: now change welcome message in group uses validation runner

--- src/commands/WelcomeMessage.ts
+++ src/commands/WelcomeMessage.ts
@@ -1,28 +1,26 @@
 import pino from 'pino';
-import { BaseCommand, Method, validateCommandProps } from '../utils/commands';
+import { BaseCommand } from '../utils/commands';
 import { ExtendedWAMessageUpdate, ExtendedWaSocket } from '../utils/messageTransformer';;
-import { GroupMetadata } from '@whiskeysockets/baileys';
-import { getWhatsAppId } from '../utils/getWhatsappId';
 import Group from '../api/models/group.model';
+import ValidationRunner from '../validators/ValidationRunner';
+import ValidateMethods from '../validators/ValidateMethods';
+import ValidateExecutorAdmin from '../validators/ValidateExecutorAdmin';
 export default class WelcomeMessage extends BaseCommand {
   async execute(message: ExtendedWAMessageUpdate, instance: ExtendedWaSocket): Promise<void> {
-    const { command, method } = message
-    if (!command) {
-      throw new Error('Command not found')
-    }
-    if (!method) {
-      throw new Error('Method not found')
-    }
-    const groupMetadata = await this.validateCommand({ command, method, instance })
-    if (!groupMetadata) {
-      return
-    }
-    if (command.args && typeof command.args === 'string') {
-      const welcomeMessage = command.args
+    const groupMetadata = await instance.groupMetadata(message.command?.groupId || '')
+    const valid = await this.validator.runValidations({
+      command: message.command,
+      metadata: groupMetadata,
+      method: message.method,
+      reply: message.reply
+    })
+    if (!valid) return
+    if (message.command?.args && typeof message.command?.args === 'string') {
+      const welcomeMessage = message.command?.args
       this.updateWelcomeMessage(groupMetadata.id, welcomeMessage)
       message.reply?.(`Mensagem de boas-vindas atualizada com sucesso`)
-    } else if (command.args && typeof command.args === 'object') {
-      const welcomeMessage = command.args.join(' ')
+    } else if (message.command?.args && typeof message.command?.args === 'object') {
+      const welcomeMessage = message.command?.args.join(' ')
       this.updateWelcomeMessage(groupMetadata.id, welcomeMessage)
       message.reply?.(`Mensagem de boas-vindas atualizada com sucesso`)
     }
@@ -41,28 +39,10 @@ export default class WelcomeMessage extends BaseCommand {
     }
   }
   private readonly logger = pino()
-  private readonly allowedMethods: Method[] = ['raw']
-  async validateCommand(props: validateCommandProps): Promise<GroupMetadata | null> {
-    if (!props.command.groupId) {
-      throw new Error('Group ID not found')
-    }
-    if (!props.command.command_executor) {
-      throw new Error('Command executor not found')
-    }
-    const whatsAppId = getWhatsAppId(props.command.command_executor)
-    const groupMetadata = await props.instance.groupMetadata(props.command.groupId)
-    if (!groupMetadata) {
-      throw new Error('Group metadata not found')
-    }
-    const isAdmin = groupMetadata.participants.find(p => p.id === whatsAppId)?.admin
-    if (isAdmin) {
-      const isAllowed = this.allowedMethods.includes(props.method)
-      this.logger.info(`Method ${isAllowed ? '' : 'not'} allowed for user ${whatsAppId}`)
-      return isAllowed ? groupMetadata : null
-    }
-    return null
-  }
+
   constructor() {
-    super('msg')
+    const adminValidator = new ValidateExecutorAdmin()
+    const methodValidator = new ValidateMethods(['raw'])
+    super('msg', new ValidationRunner([adminValidator, methodValidator]))
   }
 }
