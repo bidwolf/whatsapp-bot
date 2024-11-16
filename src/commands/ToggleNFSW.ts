669f80e5b6ab02c73cf372e8a2c5e3d15c9ee455
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2024-11-16 10:05:33 -0300

    refactor: now toggle NSFW command uses validationRunner implementation

--- src/commands/ToggleNFSW.ts
+++ src/commands/ToggleNFSW.ts
@@ -1,11 +1,12 @@
 
 import pino from 'pino';
-import { BaseCommand, Method, validateCommandProps } from '../utils/commands';
+import { BaseCommand } from '../utils/commands';
 import { ExtendedWAMessageUpdate, ExtendedWaSocket } from '../utils/messageTransformer';;
-import { GroupMetadata } from '@whiskeysockets/baileys';
 import Group from '../api/models/group.model';
 import { COMMAND_PREFIX } from '../utils/constants';
-import { getWhatsAppId } from '../utils/getWhatsappId';
+import ValidateExecutorAdmin from '../validators/ValidateExecutorAdmin';
+import ValidateMethods from '../validators/ValidateMethods';
+import ValidationRunner from '../validators/ValidationRunner';
 export default class ToggleNSFW extends BaseCommand {
   private async toggleBotStatus(groupId: string, isNSFWEnabled: boolean) { //NSFW = Not Safe For Work
     try {
@@ -25,26 +26,23 @@ export default class ToggleNSFW extends BaseCommand {
     return false
   }
   async execute(message: ExtendedWAMessageUpdate, instance: ExtendedWaSocket): Promise<void> {
-    const { command, method } = message
-    if (!command) {
-      throw new Error('Command not found')
-    }
-    if (!method) {
-      throw new Error('Method not found')
-    }
-    const groupMetadata = await this.validateCommand({ command, method, instance })
-    if (!groupMetadata) {
-      return
-    }
-    if (command.groupId) {
-      let toggleNSFWArgs = command.args
-      if (command.args && typeof command.args === 'object') {
-        toggleNSFWArgs = command.args[0]
+    const groupMetadata = await instance.groupMetadata(message.command?.groupId || '')
+    const valid = await this.validator.runValidations({
+      command: message.command,
+      metadata: groupMetadata,
+      method: message.method,
+      reply: message.reply
+    })
+    if (!valid) return
+    if (message.command?.groupId) {
+      let toggleNSFWArgs = message.command?.args
+      if (message.command?.args && typeof message.command?.args === 'object') {
+        toggleNSFWArgs = message.command?.args[0]
       }
       if (toggleNSFWArgs === 'on' || toggleNSFWArgs === 'off') {
-        const isQueryNSFWSuccessful = await this.toggleBotStatus(command.groupId, toggleNSFWArgs === 'off') // if antiPorn is off, allowNSFW is true
+        const isQueryNSFWSuccessful = await this.toggleBotStatus(message.command?.groupId, toggleNSFWArgs === 'off') // if antiPorn is off, allowNSFW is true
         if (isQueryNSFWSuccessful && message.reply) {
-          instance.sendMessage(command.groupId, {
+          instance.sendMessage(message.command?.groupId, {
             text: `Conteúdo impróprio ${toggleNSFWArgs === 'on' ? 'des' : 'h'}abilitado.`,
           })
         }
@@ -52,38 +50,16 @@ export default class ToggleNSFW extends BaseCommand {
       }
       if (message.reply) {
         this.logger.info('No args found or invalid args')
-        instance.sendMessage(command.groupId, {
+        instance.sendMessage(message.command?.groupId, {
           text: `Este comando pode ser usado da seguinte forma:\n\n*${COMMAND_PREFIX + this.command_name} on* (_Proíbe conteúdo impróprio no grupo_)\n*${COMMAND_PREFIX + this.command_name} off* (_Permite o envio de conteúdo impróprio no grupo_)`,
         })
       }
     }
   }
   private readonly logger = pino()
-  private readonly allowedMethods: Method[] = ['raw']
-  async validateCommand(props: validateCommandProps): Promise<GroupMetadata | null> {
-    if (!props.command.groupId) {
-      throw new Error('Group ID not found')
-    }
-    if (!props.command.command_executor) {
-      throw new Error('Command executor not found')
-    }
-
-    const whatsAppId = getWhatsAppId(props.command.command_executor)
-
-
-    const groupMetadata = await props.instance.groupMetadata(props.command.groupId)
-    if (!groupMetadata) {
-      throw new Error('Group metadata not found')
-    }
-    const isAdmin = groupMetadata.participants.find(p => p.id === whatsAppId)?.admin
-    if (isAdmin) {
-      const isAllowed = this.allowedMethods.includes(props.method)
-      this.logger.info(`Method ${isAllowed ? '' : 'not'} allowed for user ${whatsAppId}`)
-      return isAllowed ? groupMetadata : null
-    }
-    return null
-  }
   constructor() {
-    super('antiporn')
+    const adminValidator = new ValidateExecutorAdmin()
+    const methodValidator = new ValidateMethods(['raw'])
+    super('antiporn', new ValidationRunner([adminValidator, methodValidator]))
   }
 }
