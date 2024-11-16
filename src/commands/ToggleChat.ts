3d57744381f3c1f6a7cf1efcb650b8db590881b8
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2024-11-16 10:05:13 -0300

    refactor: now toggle chat command uses validationRunner implementation

--- src/commands/ToggleChat.ts
+++ src/commands/ToggleChat.ts
@@ -1,25 +1,23 @@
 import pino from 'pino';
-import { BaseCommand, Method, validateCommandProps } from '../utils/commands';
+import { BaseCommand } from '../utils/commands';
 import { ExtendedWAMessageUpdate, ExtendedWaSocket } from '../utils/messageTransformer';;
-import { GroupMetadata } from '@whiskeysockets/baileys';
 import { COMMAND_PREFIX } from '../utils/constants';
-import { getWhatsAppId } from '../utils/getWhatsappId';
+import ValidationRunner from '../validators/ValidationRunner';
+import ValidateMethods from '../validators/ValidateMethods';
+import ValidateExecutorAdmin from '../validators/ValidateExecutorAdmin';
 export default class ToggleChat extends BaseCommand {
   async execute(message: ExtendedWAMessageUpdate, instance: ExtendedWaSocket): Promise<void> {
-    const { command, method } = message
-    if (!command) {
-      throw new Error('Command not found')
-    }
-    if (!method) {
-      throw new Error('Method not found')
-    }
-    const groupMetadata = await this.validateCommand({ command, method, instance })
-    if (!groupMetadata) {
-      return
-    }
-    let toggleCommandArgs = command.args
-    if (command.args && typeof command.args === 'object') {
-      toggleCommandArgs = command.args[0]
+    const groupMetadata = await instance.groupMetadata(message.command?.groupId || '')
+    const valid = await this.validator.runValidations({
+      command: message.command,
+      metadata: groupMetadata,
+      method: message.method,
+      reply: message.reply
+    })
+    if (!valid) return
+    let toggleCommandArgs = message.command?.args
+    if (message.command?.args && typeof message.command?.args === 'object') {
+      toggleCommandArgs = message.command?.args[0]
     }
     if (toggleCommandArgs === 'on') {
       instance.groupSettingUpdate(groupMetadata.id, 'not_announcement')
@@ -35,28 +33,9 @@ export default class ToggleChat extends BaseCommand {
     }
   }
   private readonly logger = pino()
-  private readonly allowedMethods: Method[] = ['raw']
-  async validateCommand(props: validateCommandProps): Promise<GroupMetadata | null> {
-    if (!props.command.groupId) {
-      throw new Error('Group ID not found')
-    }
-    if (!props.command.command_executor) {
-      throw new Error('Command executor not found')
-    }
-    const whatsAppId = getWhatsAppId(props.command.command_executor)
-    const groupMetadata = await props.instance.groupMetadata(props.command.groupId)
-    if (!groupMetadata) {
-      throw new Error('Group metadata not found')
-    }
-    const isAdmin = groupMetadata.participants.find(p => p.id === whatsAppId)?.admin
-    if (isAdmin) {
-      const isAllowed = this.allowedMethods.includes(props.method)
-      this.logger.info(`Method ${isAllowed ? '' : 'not'} allowed for user ${whatsAppId}`)
-      return isAllowed ? groupMetadata : null
-    }
-    return null
-  }
   constructor() {
-    super('chat')
+    const adminValidator = new ValidateExecutorAdmin()
+    const methodValidator = new ValidateMethods(['raw'])
+    super('chat', new ValidationRunner([adminValidator, methodValidator]))
   }
 }
