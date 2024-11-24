7e6c16897a7e3c17b13d35a61d319a849d3c84f8
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2024-11-24 20:37:50 -0300

    test: create a mock implementation for a Message

---
+++ src/__mocks__/MockMessage.ts
@@ -0,0 +1,20 @@
+import { type GroupMetadata } from "@whiskeysockets/baileys/lib/Types/GroupMetadata";
+import { AvailableCommandPlatform, IMessage } from "../messages"
+import { BotCommand, Method } from "../utils/commands"
+
+export class MockMessage implements IMessage {
+  platform = AvailableCommandPlatform.WHATSAPP
+  content: string = 'test content'
+  senderId: string = 'senderID'
+  command?: BotCommand | undefined
+  groupMetadata?: GroupMetadata | undefined
+  method?: Method | undefined
+  setCommandAvailable(commandName: string) {
+    this.command = {
+      args: '',
+      command_executor: '',
+      command_name: commandName,
+      groupId: ''
+    }
+  }
+}
