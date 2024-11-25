f11b642b26e8d65aa5fd40faa982f01195b58963
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2024-11-25 13:53:22 -0300

    refactor: simplify command registration tests by using a factory function

--- src/dispatchers/commandRegistry.spec.ts
+++ src/dispatchers/commandRegistry.spec.ts
@@ -1,16 +1,25 @@
+import { MockCommand } from "../__mocks__/MockCommand"
 import { MockCommandFactory } from "../__mocks__/MockCommandFactory"
 import { ICommandFactory } from "../commands"
 import { IMessage } from "../messages"
 import { CommandRegistry } from "./CommandRegistry"
 
 describe('Command Registry', () => {
-  it('should register a command list', () => {
-    const availableCommandName = 'test'
-    const mockedFactory = new MockCommandFactory(availableCommandName)
+  const makeSut = (commandName: string) => {
+    const command = new MockCommand(commandName)
+    const mockedFactory = new MockCommandFactory(command)
     const factoryList: ICommandFactory<IMessage>[] = [
       mockedFactory
     ]
-    const commandRegistry = new CommandRegistry(factoryList)
+    const sut = new CommandRegistry(factoryList)
+    return {
+      mockedFactory,
+      sut
+    }
+  }
+  it('should register a command list', () => {
+    const availableCommandName = 'test'
+    const { mockedFactory } = makeSut(availableCommandName)
     expect(mockedFactory.called).toBe(true)
   })
   it('should return undefined when try to get a inexistent factory with a message with a command on it', async () => {
@@ -26,13 +35,9 @@ describe('Command Registry', () => {
   it('should return a factory when the command currently exists', async () => {
     //arrange
     const availableCommandName = 'test'
-    const mockedCommand = new MockCommandFactory(availableCommandName)
-    const factoryList: ICommandFactory<IMessage>[] = [
-      mockedCommand
-    ]
-    const commandRegistry = new CommandRegistry(factoryList)
+    const { sut } = makeSut(availableCommandName)
     //Act
-    const factory = commandRegistry.getFactory(availableCommandName)
+    const factory = sut.getFactory(availableCommandName)
     //Assert
     expect(factory).toBeDefined()
     expect(factory?.getCommandName()).toBe(availableCommandName)
