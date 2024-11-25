5c457bac286a23ac36df6124ffc1df3040fd42b5
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2024-11-25 06:16:46 -0300

    test: now the mocked command factory has a test property that know if get name has been called

--- src/__mocks__/MockCommandFactory.ts
+++ src/__mocks__/MockCommandFactory.ts
@@ -7,5 +7,10 @@ export class MockCommandFactory implements ICommandFactory<IMessage> {
   init(message: IMessage, instance: GroupCommunicationSocket): ICommand<IMessage> {
     return new MockCommand(this.commandName)
   }
+  getCommandName(): string {
+    this.called = true
+    return this.commandName
+  }
+  called = false
   constructor(private readonly commandName: string) { }
 }
