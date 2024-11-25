a8aaeb5d82c674c59ed83ab0949ceb4a02b8cc64
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2024-11-25 13:52:30 -0300

    feat: enhance MockCommand to support error simulation and tracking execution

--- src/__mocks__/MockCommand.ts
+++ src/__mocks__/MockCommand.ts
@@ -5,7 +5,17 @@ import { MockValidationRunner } from "./MockValidationRunner";
 export class MockCommand implements ICommand<IMessage> {
   name: string;
   validationRunner = new MockValidationRunner()
-  async run(message: IMessage): Promise<void> { }
+  async run(message: IMessage): Promise<void> {
+    this.called = true
+    if (this.isErrorExpected) {
+      throw new Error('command error')
+    }
+  }
+  called = false
+  private isErrorExpected: boolean = false
+  shouldThrow(isErrorExpected: boolean) {
+    this.isErrorExpected = isErrorExpected
+  }
   constructor(name: string) {
     this.name = name
   }
