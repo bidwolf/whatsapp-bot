8c65c45bdd6c14a5a2a62a9086ed96f5785ed5a5
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2024-11-25 06:15:01 -0300

    test: ensure that a command passed to the command list is correctly registered

--- src/dispatchers/commandRegistry.spec.ts
+++ src/dispatchers/commandRegistry.spec.ts
@@ -1,56 +1,40 @@
 import { MockCommandFactory } from "../__mocks__/MockCommandFactory"
-import { MockGroupCommunicationSocket } from "../__mocks__/MockGroupCommunicationSocket"
-import { MockMessage } from "../__mocks__/MockMessage"
 import { ICommandFactory } from "../commands"
 import { IMessage } from "../messages"
 import { CommandRegistry } from "./CommandRegistry"
 
 describe('Command Registry', () => {
-
-  it('should throw a error when try to get a factory with a message without a command on it', async () => {
-    //Arrange
+  it('should register a command list', () => {
     const availableCommandName = 'test'
-    const unavailableCommandName = 'unavailableCommand'
-    const messageWithoutCommand = new MockMessage()
-    const mockedGroupSocket = new MockGroupCommunicationSocket()
+    const mockedFactory = new MockCommandFactory(availableCommandName)
     const factoryList: ICommandFactory<IMessage>[] = [
-      new MockCommandFactory(availableCommandName)
+      mockedFactory
     ]
-    const sut = new CommandRegistry(messageWithoutCommand, mockedGroupSocket, factoryList)
-    //Act
-    const registerCommands = async () => {
-      await sut.getFactory(unavailableCommandName)
-    }
-    //assert
-    await expect(registerCommands()).rejects.toThrow('command not exists')
+    const commandRegistry = new CommandRegistry(factoryList)
+    expect(mockedFactory.called).toBe(true)
   })
   it('should return undefined when try to get a inexistent factory with a message with a command on it', async () => {
     //Arrange
-    const availableCommandName = 'test'
     const unavailableCommandName = 'unavailableCommand'
-    const mockMessage = new MockMessage()
-    mockMessage.setCommandAvailable(availableCommandName)
-    const mockedGroupSocket = new MockGroupCommunicationSocket()
     const factoryList: ICommandFactory<IMessage>[] = []
-    const commandRegistry = new CommandRegistry(mockMessage, mockedGroupSocket, factoryList)
+    const commandRegistry = new CommandRegistry(factoryList)
     //Act
-    const factory = await commandRegistry.getFactory(unavailableCommandName)
+    const factory = commandRegistry.getFactory(unavailableCommandName)
     //Assert
     expect(factory).toBeUndefined()
   })
   it('should return a factory when the command currently exists', async () => {
     //arrange
     const availableCommandName = 'test'
-    const mockMessage = new MockMessage()
-    mockMessage.setCommandAvailable(availableCommandName)
-    const mockedGroupSocket = new MockGroupCommunicationSocket()
+    const mockedCommand = new MockCommandFactory(availableCommandName)
     const factoryList: ICommandFactory<IMessage>[] = [
-      new MockCommandFactory(availableCommandName)
+      mockedCommand
     ]
-    const commandRegistry = new CommandRegistry(mockMessage, mockedGroupSocket, factoryList)
+    const commandRegistry = new CommandRegistry(factoryList)
     //Act
-    const factory = await commandRegistry.getFactory(availableCommandName)
+    const factory = commandRegistry.getFactory(availableCommandName)
     //Assert
     expect(factory).toBeDefined()
+    expect(factory?.getCommandName()).toBe(availableCommandName)
   })
 })
