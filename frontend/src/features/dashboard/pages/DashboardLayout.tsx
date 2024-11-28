9f6d5e10efb05a9e2e86d329b6dd683436a07c33
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2024-11-28 17:14:24 -0300

    feat: adicionar barra de navegação ao layout do dashboard

--- frontend/src/features/dashboard/pages/DashboardLayout.tsx
+++ frontend/src/features/dashboard/pages/DashboardLayout.tsx
@@ -1,9 +1,15 @@
 import { Outlet } from "react-router";
+import Navbar from "../../../components/Navbar";
 function DashboardLayout() {
   return (
     <>
-      <main className="w-full h-[100vh] bg-zinc-950 flex justify-center flex-col items-center text-zinc-200 gap-8">
-        <Outlet />
+      <main className="w-full h-[100vh] bg-zinc-950 flex flex-col  lg:grid grid-cols-12 text-zinc-200 gap-8">
+        <nav className="col-span-3">
+          <Navbar />
+        </nav>
+        <section className="col-span-9 m-8">
+          <Outlet />
+        </section>
       </main>
     </>
   )
