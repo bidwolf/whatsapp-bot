95e71e5c3791283b7b72b270f2fc569427da3502
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2024-11-28 17:14:51 -0300

    feat: adicionar carregamento e navegação ao componente de login

--- frontend/src/features/auth/pages/Login.tsx
+++ frontend/src/features/auth/pages/Login.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { useAuth } from "../hooks/useAuth";
-import { Navigate } from "react-router";
+import { useLocation, useNavigate, } from "react-router";
 
-export function Login() {
-  const { token, signIn } = useAuth()
+const Login = () => {
+  const { token, signIn, loading: authLoading } = useAuth()
+  const navigate = useNavigate();
   const [loading, setLoading] = React.useState(false);
+  const { state } = useLocation();
   const handleLoginSubmit: React.FormEventHandler<HTMLFormElement> = React.useCallback(async (e) => {
     e.preventDefault();
     setLoading(true)
@@ -14,19 +16,23 @@ export function Login() {
     await signIn(username, password)
     setLoading(false)
   }, [signIn]);
-  if (token) {
-    return <Navigate to={"/"} />
+  if (authLoading) {
+    return <p>Loading...</p>;
   }
+  if (token) {
+    navigate(state?.path || "/");
+  }
+
   return <React.Suspense fallback={<p>carregando</p>}>
     <main className="w-full h-[100vh] bg-black flex justify-center flex-col items-center text-zinc-200">
       <section className="p-8 border-slate-600 shadow-sm shadow-white w-3/6 min-h-1/2 bg-white/4">
-        <h1 className="text-3xl text-center text-[#8BC1EF]">Faça login</h1>
+        <h1 className="text-3xl text-center text-primary-300">Faça login</h1>
         <form onSubmit={handleLoginSubmit} className="p-4 m-4 flex flex-col gap-4">
-          <label className="text-md text-[#8BC1EF]" id="username-label">
+          <label className="text-md text-primary-300" id="username-label">
             Nome de usuário
           </label>
           <input aria-labelledby="username-label" className="p-2  text-zinc-800 placeholder:text-zinc-500" type="text" name="username" placeholder="Informe o nome de usuário" />
-          <label className="text-md text-[#8BC1EF]" id="username-password">
+          <label className="text-md text-primary-300" id="username-password">
             Senha
           </label>
           <input aria-labelledby="password-label" className="p-2  text-zinc-800 placeholder:text-zinc-500" type="password" name="password" placeholder="Informe sua senha" />
@@ -40,3 +46,4 @@ export function Login() {
     </main>
   </React.Suspense>;
 }
+export default Login
