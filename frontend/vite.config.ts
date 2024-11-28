235f3df798d3b11b4216a83028cec15355387eca
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2024-11-28 20:13:33 -0300

    fix: removing frontend static serve

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,20 +1,16 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default ({ mode }: { mode: string }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-  const config = {
-    plugins: [react()],
-    server: {
-      proxy: {
-        '/api': {
-          target: process.env.VITE_BASE_URL,
-          changeOrigin: true,
-          rewrite: (path: string) => path.replace(/^\/api/, ''),
-        },
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3333',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
-  };
-  return defineConfig(config);
-};
+  },
+});
