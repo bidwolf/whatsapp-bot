b320f00c2dff64a97cd155739287ef434b85a271
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2024-11-28 17:14:44 -0300

    feat: adicionar estado de carregamento ao contexto de autenticação

--- frontend/src/features/auth/context/auth.tsx
+++ frontend/src/features/auth/context/auth.tsx
@@ -3,6 +3,7 @@ type AuthContext = {
   token: string | null;
   signIn: (username: string, password: string) => Promise<void>;
   signOut: () => Promise<void>;
+  loading: boolean
 };
 
 export const AuthContext = React.createContext<AuthContext>({
