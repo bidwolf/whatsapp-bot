8f388d27fee58c59e925257b12b187a6bcb22038
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2024-11-28 17:14:16 -0300

    feat: atualizar rotas para gerenciamento de instâncias e grupos com layout protegido

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,17 +1,24 @@
 import { Route, Routes } from "react-router";
-import { Login } from "./features/auth/pages/Login";
+import Login from "./features/auth/pages/Login";
 import DashboardLayout from "./features/dashboard/pages/DashboardLayout";
-import DeviceRegistration from "./features/dashboard/pages/DeviceRegistration";
-import ManagementPanel from "./features/dashboard/pages/ManagementPanel";
+import InstanceRegistration from "./features/instance/pages/InstanceRegistration";
+import InstanceManagement from "./features/instance/pages/InstanceManagement";
+import ProtectedLayout from "./features/auth/pages/ProtectedLayout";
+import GroupManagement from "./features/group/pages/GroupManagement";
+import GroupRegistration from "./features/group/pages/GroupRegistration";
 
 export default function Router() {
   return (
     <Routes>
-      <Route element={
-        <DashboardLayout />
-      } >
-        <Route index element={<DeviceRegistration />} />
-        <Route path="management" element={<ManagementPanel />} />
+      <Route path="/" element={<ProtectedLayout />}>
+        <Route element={
+          <DashboardLayout />
+        } >
+          <Route index element={<InstanceManagement />} />
+          <Route path="register" element={<InstanceRegistration />} />
+          <Route path="groups/:instanceKey" element={<GroupManagement />} />
+          <Route path="groups/:instanceKey/register" element={<GroupRegistration />} />
+        </Route>
       </Route>
       <Route path="/login" element={<Login />} />
     </Routes>)
