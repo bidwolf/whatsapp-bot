1ea59d90bc9a7ce3a48bbe8ac68c10be759740be
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2024-11-28 17:12:35 -0300

    feat: adicionar configurações do Tailwind CSS e recomendações de extensões para o VSCode

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50: "#f1f7fd",
+          100: "#dfedfa",
+          200: "#c6dff7",
+          300: "#8bc1ef",
+          400: "#70afea",
+          500: "#4f91e2",
+          600: "#3a76d6",
+          700: "#3162c4",
+          800: "#2e509f",
+          900: "#2a467e",
+          950: "#1e2c4d",
+        },
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
